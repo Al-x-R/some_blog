88f20375e211a8da2468105028fb7f95a8c14b59
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-19 13:04:17 +0200

    Add home page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
 
 export default () => {
   return (
     <Switch>
-      <Route path='/' component={} exact/>
+      <Route path='/' component={HomePage} exact/>
     </Switch>
   );
 }

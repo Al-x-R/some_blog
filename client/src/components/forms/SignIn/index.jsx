0c623dd175f8314273193a417f6e81a272ad9544
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-12-22 23:58:06 +0200

    Add sign in form

---
+++ client/src/components/forms/SignIn/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import * as Yup from 'yup';
+import { Formik, Form, Field } from 'formik';
+import { Button } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
+import useStyles from './styles';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const passwordRule = [
+  /(?=.*?\d)(?=.*?[A-Z])(?=.*?[a-z])^.{8,255}$/,
+  'Your password must be at least 8 characters, and include at least one uppercase letter, and a number. ',
+];
+
+const validationSchema = Yup.object({
+  email: Yup.string().trim().email().required(),
+  password: Yup.string().matches(...passwordRule).required(),
+});
+
+const SignInForm = (props) => {
+  const { onSubmit } = props;
+  const classes = useStyles();
+
+  const handleSubmit = (values) => {
+    onSubmit(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}>
+      {({ submitForm, isSubmitting }) => (
+        <Form className={classes.root}>
+
+          <Field
+            component={TextField}
+            name="email"
+            type="email"
+            label="Email"
+            variant="outlined"
+          />
+          <Field
+            id="outlined-password-input"
+            component={TextField}
+            type="password"
+            label="Password"
+            name="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            disabled={isSubmitting}
+            onClick={submitForm}
+          >
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default SignInForm;
